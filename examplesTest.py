4103d4fb762740ed5a98c222fb117e8711817ff8
harshp96 <padhyaharsh2@gmail.com>
2023-08-02 04:44:35 +0530

    updated logging support

--- examplesTest.py
+++ examplesTest.py
@@ -7,6 +7,13 @@ import json
 from datetime import date, timedelta
 from time import sleep
 
+logging.basicConfig(
+    level=logging.INFO,  # Set the log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Define the log message format
+    filename="debug.log",  # Specify the log file name
+    filemode="a"  # 'w' mode will overwrite the log file on each run, 'a' will append
+)
+
 with open('API_Keys.json', "r") as file:
     api_keys = json.load(file)
 
@@ -29,6 +36,7 @@ try:
         else:
             expiry_date = today + timedelta(days=3-today.weekday())     # this week's thursday
         expiry_date = expiry_date.strftime('%d-%m-%Y')
+        logging.info('Taking expiry_date: {}'.format(expiry_date))
         print('Taking expiry_date: {}'.format(expiry_date))
     
     ce_holdPrice = 0
@@ -40,6 +48,7 @@ try:
             df = pd.json_normalize(data['data']['results'])
             if strick_price == None:
                 strick_price = round(float(df['spot_price'].unique()[0])/50)*50
+                logging.info('Taking strick_price: {}'.format(strick_price))
                 print('Taking strick_price: {}'.format(strick_price))
             cedf = df[(df.stk_price == (strick_price+50))]
 
@@ -54,17 +63,26 @@ try:
                 ce_holdPrice = ce_price
                 pe_holdPrice = pe_price
                 havePosition = True
+                logging.info('=====================================/n Making entry position /n=====================================')
+                logging.info('[CALL] Buy Price: {0}, Buy Quntity: {1}, For Strick Price: {2}'.format(ce_holdPrice, holdQuantity, strick_price+50))
+                logging.info('[PUT] Buy Price: {0}, Buy Quntity: {1}, For Strick Price: {2}'.format(pe_holdPrice, holdQuantity, strick_price))
+
                 print('=====================================/n Making entry position /n=====================================')
                 print('[CALL] Buy Price: {0}, Buy Quntity: {1}, For Strick Price: {2}'.format(ce_holdPrice, holdQuantity, strick_price+50))
                 print('[PUT] Buy Price: {0}, Buy Quntity: {1}, For Strick Price: {2}'.format(pe_holdPrice, holdQuantity, strick_price))
 
+            logging.info('Call [{0}->{1}], PUT [{2}->{3}], Current Position: {4}'.format(ce_holdPrice, ce_price, pe_holdPrice, pe_price, 
+                round((ce_price-ce_holdPrice)*holdQuantity+(pe_price-pe_holdPrice)*holdQuantity)
+            ))
             print('Call [{0}->{1}], PUT [{2}->{3}], Current Position: {4}'.format(ce_holdPrice, ce_price, pe_holdPrice, pe_price, 
                 round((ce_price-ce_holdPrice)*holdQuantity+(pe_price-pe_holdPrice)*holdQuantity)
             ))
 
-        except Exception as ex:
-            print("Error: {}".format(ex))
+        except Exception as e:
+            logging.error("Error: {}".format(e))
+            print("Error: {}".format(e))
 
-        sleep(30)
+        sleep(5)
 except Exception as e:
-    print("Error : {}".format(e))
+    logging.error("Error: {}".format(e))
+    print("Error: {}".format(e))
